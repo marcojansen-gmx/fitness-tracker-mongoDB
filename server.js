019a1ed07bce0875c21559e83e9228a16472bb61
Marco <Marco.jansen@gmx.us>
2021-03-21 13:43:40 +0800

    added models

--- server.js
+++ server.js
@@ -12,8 +12,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/workout', { useNewUrlParser: true }).
-  catch(error => handleError(error));
+mongoose.connect('mongodb://localhost/workout', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("we are connected");
+});
 
 // routes
 app.use(require("./routes/api-routes.js"));
