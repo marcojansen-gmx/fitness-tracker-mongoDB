51fabdc18abf592a9a2847c6876d61112358730e
Marco <Marco.jansen@gmx.us>
2021-03-30 20:22:36 +0800

    added dotenv to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
